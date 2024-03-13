5d69e5c278af08b1b5b20b66624edfaf46437d26
Yallison Gabriel Reis <gabrielyallison2012@hotmail.com>
2024-03-13 01:49:29 -0400

    Add namespaces

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 // Register the Composer autoloader...
 require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/src/CepApi.php';
-require __DIR__.'/src/Providers/BrasilApi.php';
+/*require __DIR__.'/src/CepApi.php';
+require __DIR__.'/src/Providers/BrasilApi.php';*/
 
-CepApi::getAddress('01501-040');
+
+Ygreis\CepApiPhp\CepApi::getAddress('01501-040');
--- src/CepApi.php
+++ src/CepApi.php
@@ -1,4 +1,7 @@
 <?php
+declare(strict_types=1);
+
+namespace Ygreis\CepApiPhp;
 
 class CepApi
 {
